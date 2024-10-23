d7302d72c3cf3ab32484f15e3d4f2fe1bbd6bce9
Juan <75174627@continental.edu.pe>
2024-10-23 00:44:51 -0500

    Caso 1:Caso sin elementos en la lista debe lanzar una excepción(Verde)

--- src/logica/DesviacionEstandar.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from src.logica.Promedio import Promedio
-
-class PruebaOperaciones(unittest.TestCase):
-
-   def test_Promedio_vacio_retornaNone(self):
-      elementos =Promedio([])
-      self.assertIsNone(elementos.media())
--- src/logica/Promedio.py
+++ src/logica/Promedio.py
@@ -1,7 +1,12 @@
+class NoSePuedeCalcular(Exception):
+    pass
 class Promedio:
 
     def __init__(self,elementos):
         self.__elementos = elementos
 
     def media(self):
-        return False
+        try:
+            pass
+        except  NoSePuedeCalcular:
+            return None
