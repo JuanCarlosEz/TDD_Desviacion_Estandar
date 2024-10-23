9f843d589740a62208a646d9f01d7854864819f1
Juan <75174627@continental.edu.pe>
2024-10-23 18:45:29 -0500

    Caso 3:Dos elementos en la lista(Rojo)

--- test/PruebaOperaciones.py
+++ test/PruebaOperaciones.py
@@ -9,3 +9,6 @@ class PruebaOperaciones(unittest.TestCase):
    def test_Promedio_unElemento(self):
         elementos = Promedio([6])
         self.assertEqual(elementos.media(), 6)
+   def test_Promedio_de_dos_elementos(self):
+        elementos = Promedio([2,6])
+        self.assertEqual(elementos.media(), 4)
