a245af3523c3dd31188216ef272481a06e87ebb6
Juan <75174627@continental.edu.pe>
2024-10-23 21:31:15 -0500

    Caso 7:Promedio con n elementos, donde hay elementos que no son numéricos(Rojo)

--- test/PruebaOperaciones.py
+++ test/PruebaOperaciones.py
@@ -22,3 +22,7 @@ class PruebaOperaciones(unittest.TestCase):
    def test_Promedio_N_elementos_positivos_y_negativos(self):
        elementos = Promedio([-3, 8, 11, -1, -5])
        self.assertEqual(((8 +11) -(3+1+5))/5, elementos.media())
+   def test_Promedio_N_elementos_elementos_no_numericos(self):
+       elementos = Promedio(["a","b","c"])
+       self.assertEqual("TypeError",elementos.media())
+
