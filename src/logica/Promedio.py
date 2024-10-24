99e43925b8bf70eb3dd840c69df5e95e5784bb30
Juan <75174627@continental.edu.pe>
2024-10-23 22:28:16 -0500

    Caso 7:Promedio con n elementos, donde hay elementos que no son numéricos(Azul)

--- src/logica/Promedio.py
+++ src/logica/Promedio.py
@@ -1,29 +1,31 @@
 class NoSePuedeCalcular(Exception):
     pass
+
+
 class Promedio:
 
-    def __init__(self,elementos):
+    def __init__(self, elementos):
         self.__elementos = elementos
 
     def media(self):
         try:
-            if len(self.__elementos) == 0:
+            if not self.__elementos:
                 raise NoSePuedeCalcular("No se puede calcular el promedio de una lista vacía")
-            else:
-                if not all(isinstance(x, (int, float)) for x in self.__elementos):
-                    raise TypeError("Todos los elementos deben ser números")
-                if all(x == 0 for x in self.__elementos):
-                    return "Cero"
-                if len(self.__elementos) == 1:
-                    return (self.__elementos[0])
-                if len(self.__elementos) == 2:
-                    return sum(self.__elementos)/2
-                if len(self.__elementos) > 2:
-                    suma_positivos = sum(x for x in self.__elementos if x > 0)
-                    suma_negativos = sum(abs(x) for x in self.__elementos if x < 0)
-                    return (suma_positivos - suma_negativos) /len(self.__elementos)
 
-        except  NoSePuedeCalcular:
+            if not all(isinstance(x, (int, float)) for x in self.__elementos):
+                raise TypeError("Todos los elementos deben ser números")
+
+            if all(x == 0 for x in self.__elementos):
+                return "Cero"
+
+            if len(self.__elementos) == 1:
+                return self.__elementos[0]
+            if len(self.__elementos) >=2:
+                suma_positivos = sum(x for x in self.__elementos if x > 0)
+                suma_negativos = sum(abs(x) for x in self.__elementos if x < 0)
+                return (suma_positivos - suma_negativos) / len(self.__elementos)
+
+        except NoSePuedeCalcular:
             return None
         except TypeError:
             return "TypeError"
