2d820851316c93c2f84aa028476e45b241462dae
Tatiana <75193605@continental.edu.pe>
2024-10-24 00:42:32 -0500

    Caso 2.2:Desviacion estandar un solo elemento(Verde)

--- src/logica/DesviacionEstandar.py
+++ src/logica/DesviacionEstandar.py
@@ -8,6 +8,7 @@ class DesviacionEstandar:
         try:
             if len(self.__elementos) == 0:
                 raise NoSePuedeCalcular("No se puede calcular la desviacion estandar de una lista vacía")
-
+            if len(self.__elementos) == 1:
+                return 0
         except NoSePuedeCalcular:
             return None
